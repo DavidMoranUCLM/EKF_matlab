228b2bff804ac4c9e00dea4595e2f3b53843f9b9
David <david.moran@alu.uclm.es>
2025-04-07 12:31:45 +0200

    get_H added

---
+++ +kalman/get_H.m
@@ -0,0 +1,12 @@
+function H = get_H(q,g,r)
+%GETH Summary of this function goes here
+%   Detailed explanation goes here
+
+
+ug = cross(g,q(2:4));
+ur = cross(r,q(2:4));
+
+H = 2*[ug utils.skewMatrix(ug+q(1)*g) + dot(g,q(2:4))*eye(3)-g*q(2:4)';
+       ur utils.skewMatrix(ur+q(1)*r) + dot(r,q(2:4))*eye(3)-r*q(2:4)'];
+end
+
